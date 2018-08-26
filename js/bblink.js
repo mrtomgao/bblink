738707acc5c3fa4245701a217369b0debda4d696
mrtomgao <34082794+sjhou@users.noreply.github.com>
2018-08-25 23:25:36 -0700

    ni

--- js/bblink.js
+++ js/bblink.js
@@ -1,17 +1,15 @@
-$(document).ready(function() {
+  $(document).ready(function() {
 
-  var bbUser = {};
-      bbUser ["username"] = "tomgao"
-      bbUser ["room"] = "chill"
-      bbUser ["avatar"] = "http://www.lol.com/img.png"
+    var bbUser;
+    if (typeof $.cookie('bbUser') === 'undefined'){
+      // similar behavior as an HTTP redirect
+      window.location.replace("login.html");
+    } else {
+      bbUser = JSON.parse($.cookie('bbUser'));
+    }
 
-	if (typeof $.cookie('bbUser') === 'undefined'){
-    // similar behavior as an HTTP redirect
-    window.location.replace("login.html");
-	} else {
+    alert(bbUser.username);
 
-	}
-		
     loadMessages();
 
     $("#idSend").click(function() {
@@ -20,86 +18,86 @@ $(document).ready(function() {
       }
       else 
       {
-          $.ajax({
-             type: "POST",
-             contentType: "application/x-www-form-urlencoded",
-             url: "https://bblinkapi.azurewebsites.net/msg",     
-             data: { body: $("#idMessageBox").val().trim(), user: "raekwon", channel: "lockedtite" },
-             success: function(data) {
-                loadMessages();
-                  $("#idMessageBox").val('');
-              },
-              error: function(data) {
-                  console.log("error ", data.error);
-              },
-              dataType: "json"               
-          });
+        $.ajax({
+         type: "POST",
+         contentType: "application/x-www-form-urlencoded",
+         url: "https://bblinkapi.azurewebsites.net/msg",     
+         data: { body: $("#idMessageBox").val().trim(), user: "raekwon", channel: "lockedtite" },
+         success: function(data) {
+          loadMessages();
+          $("#idMessageBox").val('');
+        },
+        error: function(data) {
+          console.log("error ", data.error);
+        },
+        dataType: "json"               
+      });
       }
     });
 
-});
+  });
 
-function timeAgo(date) {
+  function timeAgo(date) {
 
-  var seconds = Math.floor((new Date() - date) / 1000);
-  var interval = Math.floor(seconds / 31536000);
+    var seconds = Math.floor((new Date() - date) / 1000);
+    var interval = Math.floor(seconds / 31536000);
 
-  if (interval > 1) {
-    return interval + " years";
+    if (interval > 1) {
+      return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+      return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+      return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+      return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+      return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
   }
-  interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
-    return interval + " months";
-  }
-  interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
-    return interval + " days";
-  }
-  interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
-    return interval + " hours";
-  }
-  interval = Math.floor(seconds / 60);
-  if (interval > 1) {
-    return interval + " minutes";
-  }
-  return Math.floor(seconds) + " seconds";
-}
 
-function loadMessages() {
+  function loadMessages() {
 
     var me = 'raekwon';
     $("#idReader").empty();
 
     $.ajax({
-        url: "https://bblinkapi.azurewebsites.net/msgafter"
+      url: "https://bblinkapi.azurewebsites.net/msgafter"
     }).then(function(data) {
-    var count = data.length;
-    for (i = 0; i < count; i++) {
-     var createDate = new Date(data[i].Created_date);
-     var msgbody = "";
-     if (data[i].user != me) 
-     {
+      var count = data.length;
+      for (i = 0; i < count; i++) {
+       var createDate = new Date(data[i].Created_date);
+       var msgbody = "";
+       if (data[i].user != me) 
+       {
         msgBody = "<div id=" + data[i]._id + " class=incoming_msg>" +
-                  "<div class=incoming_msg_img><img src=https://mrtomgao.github.io/hello/images/avatar.png></div>" +
-                  "<div class=received_msg>" +
-                  "<div class=received_withd_msg>" +
-                  "<p>" + data[i].body + "</p>" +
-                  "<span class=time_date>" + timeAgo(createDate) + " ago</span>" +
-                  "</div></div></div>";
-     } 
-     else 
-     {
+        "<div class=incoming_msg_img><img src=https://mrtomgao.github.io/hello/images/avatar.png></div>" +
+        "<div class=received_msg>" +
+        "<div class=received_withd_msg>" +
+        "<p>" + data[i].body + "</p>" +
+        "<span class=time_date>" + timeAgo(createDate) + " ago</span>" +
+        "</div></div></div>";
+      } 
+      else 
+      {
         msgBody = "<div id=" + data[i]._id + " class=outgoing_msg>" +                  
-                  "<div class=sent_msg_img><img src=https://mrtomgao.github.io/hello/images/avatar.png></div>" +                                  
-                  "<div class=sent_msg>" +
-                  "<p>" + data[i].body + "</p>" +
-                  "<span class=time_date>" + timeAgo(createDate) + " ago</span>" +
-                  "</div></div>";
-     }
-        $(msgBody).appendTo("#idReader");
-        $('#idReader').scrollTop($('#idReader')[0].scrollHeight);
+        "<div class=sent_msg_img><img src=https://mrtomgao.github.io/hello/images/avatar.png></div>" +                                  
+        "<div class=sent_msg>" +
+        "<p>" + data[i].body + "</p>" +
+        "<span class=time_date>" + timeAgo(createDate) + " ago</span>" +
+        "</div></div>";
+      }
+      $(msgBody).appendTo("#idReader");
+      $('#idReader').scrollTop($('#idReader')[0].scrollHeight);
     }
-    });
+  });
 
-}
+  }
--- js/login.js
+++ js/login.js
@@ -1,36 +1,29 @@
 $(document).ready(function() {
-  // Form Submission
+
   $("#lg_submit").click(function() {        
-    
+
       var username = $('#lg_username').val();
       var room = $('#lg_room').val();
       var avatar = $('#lg_avatar').val();
 
       if (username != '' && room != '' && avatar != '') 
       {
-        //clear the form
-        $('#lg_username').val('');
-        $('#lg_room').val('');
-        $('#lg_avatar').val('');        
 
-        bbUserJson = [];
-
-        item = {}
-        item ["username"] = username;
-        item ["room"] = room;
-        item ["avatar"] = avatar;
-        bbUserJson.push(item);
         
-        $.cookie("bbUser", JSON.stringify(bbUserJson));
+        bbUser = {}
+        bbUser ["username"] = username;
+        bbUser ["room"] = room;
+        bbUser ["avatar"] = avatar;
+        $.cookie("bbUser", JSON.stringify(bbUser));
 
-        if (typeof $.cookie('bbUser') != 'undefined')
+        if (typeof $.cookie("bbUser") != 'undefined')
         {
           //good to go!!
-          window.location.replace("index.html");
+          window.location.replace('index.html');
         }
         else
         {
-          alert('error creating login object');
+          alert('error creating login object');                   
         }
       }
       else 
